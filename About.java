1df0b59b89458ee217a2872b943989d292b927b3
Shubham <thakursingh1929@gmail.com>
2021-08-11 18:25:09 +0530

    resolved setting page issue

--- About.java
+++ About.java
@@ -15,12 +15,12 @@ class About extends JPanel
       setBackground(c);
       Font f1=new Font("Algerian", Font.ITALIC,25);
 	  
-	  img2=new ImageIcon("bg2.jpg");
+	  img2=new ImageIcon("IMAGES/bg2.jpg");
 	  bg1= img2.getImage();
-	  img3=new ImageIcon("abt2.png");
+	  img3=new ImageIcon("IMAGES/abt2.png");
 	  bg2= img3.getImage();
       
-	  img1=new ImageIcon("back1.jpg");
+	  img1=new ImageIcon("IMAGES/back1.jpg");
 	  b1=new JButton(img1);
 	  b1.setBackground(new Color(128,128,255));
       b1.setBounds(730,600,125,40);
